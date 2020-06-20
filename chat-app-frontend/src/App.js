2a9a6d2a16cac671ab2e7f8c06527aa587b76c74
supercode0228 <bee0228angel@outlook.com>
2020-06-20 12:13:02 -0400

    Upgrade the App and Room page.

--- chat-app-frontend/src/App.js
+++ chat-app-frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import configureStore from './store';
 import routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+		<Provider store={configureStore()}>
+			<BrowserRouter>
 				<Switch>
 					{
 						routes.map(route => {
@@ -20,7 +23,7 @@ function App() {
 					}
 				</Switch>
 			</BrowserRouter>
-    </div>
+		</Provider>
   );
 }
 
--- chat-app-frontend/src/pages/ChatRoomPage.js
+++ chat-app-frontend/src/pages/ChatRoomPage.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux"; 
 import { SideBar } from '../components/ChatRoom'
+
+import { roomActions } from '../actions';
 class ChatRoomPage extends Component {
+	componentDidMount() {
+		this.props.roomActions.fetchRoomsRequest();
+	}
+	
 	render() {
 		return (
 			<div className="chat-room">
@@ -16,4 +24,16 @@ class ChatRoomPage extends Component {
 	}
 }
 
-export default ChatRoomPage;
+const mapStateToProps = state => {
+  return {
+    rooms: state.rooms,
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    roomActions: bindActionCreators(roomActions, dispatch)
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChatRoomPage);
