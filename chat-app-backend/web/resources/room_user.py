78160dd66855ea15967b1cf57ea353ae8ca14881
supercode0228 <bee0228angel@outlook.com>
2020-06-22 06:10:15 -0400

    upgraded the api for postRoomUser.

--- chat-app-backend/web/resources/room_user.py
+++ chat-app-backend/web/resources/room_user.py
@@ -1,9 +1,11 @@
 from flask import request
 from flask_restful import fields, Resource
 from ..models.room_user import RoomUserModel, RoomUserSchema
+from ..models.user import UserModel, UserSchema
 from .util import custom_response
 
 room_user_schema = RoomUserSchema()
+user_schema = UserSchema()
 
 class AllRoomUsers(Resource):
 
@@ -12,16 +14,37 @@ class AllRoomUsers(Resource):
 		status_msg, status_code, result = '', 200, {}
 		try:
 			req_data = request.get_json()
-			data = room_user_schema.load(req_data)
-			room_user = RoomUserModel(data)
-			room_user.save()
-			result = room_user_schema.dump(room_user)
-			status_code = 200
-			status_msg = 'GetRoomUserSuccess'
 
+			user = user_schema.dump(UserModel.get_user_by_email(req_data['email']))
+			user_id = user['id']
+
+			if user_id :
+				room_id = req_data['room_id']
+				room_user = RoomUserModel.get_room_by_id(user_id, room_id)
+				if room_user :
+					result = room_user_schema.dump(room_user)
+					status_code = 200
+					status_msg = 'GetRoomUserSuccess'
+				else :
+					user_name = req_data['user_name']
+					params = {
+						'user_id': user_id,
+						'room_id': room_id,
+						'user_name': user_name
+					}
+					data = room_user_schema.load(params)
+					room_user = RoomUserModel(data)
+					room_user.save()
+					result = room_user_schema.dump(room_user)
+					status_code = 200
+					status_msg = 'GetRoomUserSuccess'
+			else :
+				status_code = 500
+				status_msg = 'GetUserNotExist'
 		except Exception as e:
 			status_code = 500
 			status_msg = 'GetRoomUserError'
+			print(format(str(e)))
 		
 		finally:
 			return custom_response(result, status_code, status_msg)
