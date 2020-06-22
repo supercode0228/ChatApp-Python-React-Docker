2800981798562f1e647cd27293c9f66850776bb4
supercode0228 <bee0228angel@outlook.com>
2020-06-22 11:23:31 -0400

    Integrate socket.io to react.

--- chat-app-frontend/src/App.js
+++ chat-app-frontend/src/App.js
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import configureStore from './store';
 import routes from './routes';
 
-function App() {
-  return (
-		<Provider store={configureStore()}>
-			<BrowserRouter>
-				<Switch>
-					{
-						routes.map(route => {
-							return(
-								<Route
-									path={route.path}
-									component={route.component}
-									key={route.name}
-								/>
-							);
-						})
-					}
-				</Switch>
-			</BrowserRouter>
-		</Provider>
-  );
+import {API_URL} from './constants';
+
+import io from 'socket.io-client';
+
+class App extends Component {
+
+	componentDidMount() {
+		const socket = io.connect(API_URL);
+		socket.emit('connected');
+	}
+
+	render() {
+		return (
+			<Provider store={configureStore()}>
+				<BrowserRouter>
+					<Switch>
+						{
+							routes.map(route => {
+								return(
+									<Route
+										path={route.path}
+										component={route.component}
+										key={route.name}
+									/>
+								);
+							})
+						}
+					</Switch>
+				</BrowserRouter>
+			</Provider>
+		);
+	}
 }
 
 export default App;
