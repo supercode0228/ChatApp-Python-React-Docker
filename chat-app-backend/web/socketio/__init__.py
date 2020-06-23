f84adfd4ac29c48ac1fa03b3ccc09a5370bb4310
supercode0228 <bee0228angel@outlook.com>
2020-06-22 20:37:29 -0400

    Defined socket io event for chat.

--- chat-app-backend/web/socketio/__init__.py
+++ chat-app-backend/web/socketio/__init__.py
@@ -1,7 +1,11 @@
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 socketio = SocketIO()
 
 @socketio.on('connected')
 def on_connect():
 	print('user connected')
+
+@socketio.on('chat_send')
+def on_chat_send(data):
+	emit('chat_received', data, broadcast=True)
